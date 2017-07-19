fde83d8b67bcff182ac3dfa422aa202aaaca409c
Trevor Lemon <duhlemman@gmail.com>
2017-07-18 20:51:37 -0700

    first commit

---
+++ src/components/WoF/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { array } from 'prop-types'
+
+class WoF extends Component {
+  constructor(props={}){
+    super(props)
+  }
+  
+  render(){
+    const { items } = this.props;
+    
+    return (
+      <div className="whos-on-first">
+        {items.map((data, ndx) => {
+          return <div key={ ndx }>{data[0]} on {data[1]}</div>
+        })}
+      </div>
+    )
+  }
+}
+
+WoF.propTypes = {
+  items: array
+}
+
+WoF.defaultProps = {
+  items: [
+    ["Who's", "first"],
+    ["What's", "second"],
+    ["I Don't Know is", "third"],
+  ]
+}
+
+export default WoF
